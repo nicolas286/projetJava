be496db0d7ffd587b2f0966518a1141e956a62e7
Feral Web Designer <155650119+nicolas286@users.noreply.github.com>
2026-04-11 10:59:07 +0200

    Refactor OrderLineDataAccess interface
    Removed comment about GenericDao coverage for insert method.

--- src/dataAccessPackage/OrderLineDataAccess.java
+++ src/dataAccessPackage/OrderLineDataAccess.java
@@ -5,9 +5,10 @@ import modelPackage.OrderLine;
 
 import java.util.List;
 
-public interface OrderLineDataAccess {
+public interface OrderLineDataAccess //extends GenericDao... {
 
     void insert(OrderLine orderLine) throws DataAccessException;
+    // Tu peux retirer l'insert qui est couvert par générique DAO
 
     List<OrderLine> getLinesByOrderId(int orderId) throws DataAccessException;
 }
