eeb441ff51351b91014adc622762638dbf8c3242
Feral Web Designer <155650119+nicolas286@users.noreply.github.com>
2026-04-11 10:56:11 +0200

    Refactor OrderDataAccess to keep only specific method
    Retain OrderDataAccess.java for getOrdersByTableId method only, as it duplicates functionality from genericDAO.

--- src/dataAccessPackage/OrderDataAccess.java
+++ src/dataAccessPackage/OrderDataAccess.java
@@ -13,3 +13,6 @@ public interface OrderDataAccess extends GenericDAO<Order, Integer> {
 
     List<Order> getOrdersByTableId(int tableId) throws DataAccessException;
 }
+
+// Ce fichier ne me paraît pas nécessaire tel quel dans le sens où il reprend essentiellement la même chose que notre genericDAO (getAllOrders = findAll, getOrderById = findById).
+// ok pour getOrdersByTableId, donc on ne garderait ce fichier que pour cet élément spécifique aux commandes
