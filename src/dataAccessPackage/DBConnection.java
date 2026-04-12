6d63a0baef6044e16d11f57f3d2b657d18b821f1
Feral Web Designer <155650119+nicolas286@users.noreply.github.com>
2026-04-12 12:38:09 +0200

    Refactor DBConnection to use Singleton pattern
    Implemented Singleton pattern for DBConnection class to ensure a single instance throughout the application. Added connection retrieval methods with fallback options for database credentials.

--- src/dataAccessPackage/DBConnection.java
+++ src/dataAccessPackage/DBConnection.java
@@ -4,10 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/* 
+Pattern Singleton :
+Une seule instance de DBConnection dans toute l'application
+*/
+
 public class DBConnection {
 
     private static DBConnection instance;
     private final Connection connection;
+
+    /* 
+    Récupération des informations de connexion à la db
+    System.getProperty permet de passer des arguments au lancement du programme, sinon fallback 
+    */
 
     private static final String URL =
             System.getProperty("db.url", "jdbc:mysql://localhost:3306/projet_java");
@@ -15,6 +25,8 @@ public class DBConnection {
             System.getProperty("db.user", "root");
     private static final String PASSWORD =
             System.getProperty("db.password", "willylechat");
+
+    /* Méthode centrale de connexion à la DB avec les infos */
 
     private DBConnection() throws SQLException {
         try {
@@ -24,12 +36,22 @@ public class DBConnection {
         }
     }
 
+    /* 
+    Méthode publique :
+    - crée l'instance si elle n'existe pas
+    - synchronized évite la création de plusieurs instances en cas d'accès concurrent (multi-thread)
+    */
+
     public static synchronized DBConnection getInstance() throws SQLException {
         if (instance == null) {
             instance = new DBConnection();
         }
         return instance;
     }
+
+    /* 
+    Retourne la connexion JDBC gérée par le Singleton
+    */
 
     public Connection getConnection() {
         return connection;
