c1b4d55b1755fdeab2c2f4b6bdef3f2207827843
Feral Web Designer <155650119+nicolas286@users.noreply.github.com>
2026-04-12 13:01:55 +0200

    Change getConnection to return DBConnection instance
    Updated getConnection method to return DBConnection instance instead of a Connection object.

--- src/dataAccessPackage/AbstractDAO.java
+++ src/dataAccessPackage/AbstractDAO.java
@@ -5,11 +5,17 @@ import exceptionPackage.DataAccessException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/* 
+Classe abstraite :
+- centralise l'accès à la connexion
+- évite la duplication de code dans les DAO
+*/
+
 public abstract class AbstractDAO<T, ID> implements GenericDAO<T, ID> {
 
     protected Connection getConnection() throws DataAccessException {
         try {
-            return DBConnection.getInstance().getConnection();
+            return DBConnection.getInstance();
         } catch (SQLException e) {
             throw new DataAccessException("Error while getting database connection.", e);
         }
